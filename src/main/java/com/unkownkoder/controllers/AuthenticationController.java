8ec56cfcfc263e1af1eb87321df6ab1ed1584a54
OFD5 <mynetbooks54@gmail.com>
2023-11-08 18:48:07 +0200

    first commit

---
+++ src/main/java/com/unkownkoder/controllers/AuthenticationController.java
@@ -0,0 +1,27 @@
+package com.unkownkoder.controllers;
+
+import com.unkownkoder.models.ApplicationUser;
+import com.unkownkoder.models.LoginResponseDTO;
+import com.unkownkoder.models.RegistrationDTO;
+import com.unkownkoder.services.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin("*")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ApplicationUser registerUser(@RequestBody RegistrationDTO body){
+        return authenticationService.registerUser(body.getUsername(), body.getPassword());
+    }
+    
+    @PostMapping("/login")
+    public LoginResponseDTO loginUser(@RequestBody RegistrationDTO body){
+        return authenticationService.loginUser(body.getUsername(), body.getPassword());
+    }
+}   
